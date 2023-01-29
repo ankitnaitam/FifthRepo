30097c68e6c7f56182b24beff2289abba4b36b77
ankitnaitam <ankitnaitam2015@gmail.com>
2023-01-29 17:31:59 +0530

    changes by Ankit

---
+++ src/main/java/com/BikkadIT/GitDemo5Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemo5Application.class, args);
+	}
+
+}
